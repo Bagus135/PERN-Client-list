712f7e1aa287d2b282454f44d12bfcb53d9f3cc0
Bagus135 <bagusmustaqim35@gmail.com>
2024-07-17 23:34:47 +0700

    finaaallll

--- src/component/Home/admin.jsx
+++ src/component/Home/admin.jsx
@@ -1,9 +1,9 @@
-import { getIMG } from "../../img/utiilsIMg"
+import { Img } from "../../img/utiilsIMg"
+const foto = Img.foto
+const facebook = Img.facebook
+const ig = Img.ig
+const github = Img.github
 
-const foto = getIMG("../../img/Ayanokouji.jpg")
-const facebook = getIMG("../../img/facebook.png")
-const ig = getIMG("../../img/instagram.png")
-const github = getIMG("../../img/github.jpg")
 
 function Admin(){
     return(
@@ -12,7 +12,7 @@ function Admin(){
             className="btn btn-primary" 
             data-toggle="modal"
             data-target={`#idAdmin`}>
-            Edit
+            Admin
             </button>
 
             <div className="modal" id={`idAdmin`}>
@@ -20,21 +20,24 @@ function Admin(){
                 <div className="modal-content">
 
                 <div className="modal-header">
-                    <h4 className="modal-title">Edit Menu</h4>
+                    <h4 className="modal-title">ADMIN</h4>
                     <button type="button" className="close" data-dismiss="modal">&times;</button>
                 </div>
 
                 <div className="modal-body">
-                    <h2 className="text-center mt-5">ABOUT AUTHOR</h2>
-                    <img src={foto}/>
+                    <div className="container-fluid">
+                    <h2 className="text-center">ABOUT AUTHOR</h2>
+                    <img className="w-100" src={foto}/>
                     <h3 className="text-center mt-5">Bagus Mustaqim</h3>
+                    <hr className="bg-dark"/>
                     <p>Ampun puhh.... Masih pemula puhh. Saya masih kroco puh. Saya belum ada apa apanya dibandingkan sepuh sekalian</p>
-                    <h3 className='text-center mt-5'>Follow Me</h3>
+                    <h3 className='text-center mt-5 mb-3'>Follow Me</h3>
 
-                    <div className='Sosmed'>
-                    <a href='https://www.facebook.com/bagus.mustaqim.56?mibextid=ZbWKwL' target='blank'><img src={facebook} title='Facebook'/></a>
-                    <a href='https://www.instagram.com/bagustaqim_?igsh=ZWxwajlsbHl2dGlx'target='blank'><img src={ig} title='Instagram'/></a>
-                    <a href='https://github.com/Bagus135' target='blank'><img src={github} title="Github"/></a>
+                    <div className='d-flex justify-content-center'>
+                    <a href='https://www.facebook.com/bagus.mustaqim.56?mibextid=ZbWKwL' target='blank'><img className="w-75" src={facebook} title='Facebook'/></a>
+                    <a href='https://www.instagram.com/bagustaqim_?igsh=ZWxwajlsbHl2dGlx'target='blank'><img className="w-75" src={ig} title='Instagram'/></a>
+                    <a href='https://github.com/Bagus135' target='blank'><img className="w-75" src={github} title="Github"/></a>
+                    </div>
                     </div>
                 </div>
 
--- src/img/utiilsIMg.jsx
+++ src/img/utiilsIMg.jsx
@@ -1,5 +1,11 @@
 
-export function getIMG(path){
+function getIMG(path){
     return new URL(path, import.meta.url).href
 }
+export const Img = {
+    foto : getIMG("./Ayanokouji.jpg"),
+    facebook : getIMG("./facebook.png"),
+    ig : getIMG("./instagram.png"),
+    github : getIMG("./github.png")
+} 
 
